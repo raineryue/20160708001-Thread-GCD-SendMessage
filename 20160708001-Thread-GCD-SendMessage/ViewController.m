61997f93b5b5f80187e28fab8bfb468a3397ae24
Rainer <it_jinlei.yue@163.com>
2016-07-08 16:10:01 +0800

    线程知识点梳理：使用GCD了解线程之间的通讯，主线程和子线程之前发消息
    线程知识点梳理：使用GCD了解线程之间的通讯，主线程和子线程之前发消息

--- 20160708001-Thread-GCD-SendMessage/ViewController.m
+++ 20160708001-Thread-GCD-SendMessage/ViewController.m
@@ -10,18 +10,36 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self showDownLoadImage];
+}
+
+- (void)showDownLoadImage {
+    // 使用 异步函数 + 并行队列 下载图片
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        // 获取图片地址
+        NSURL *imageUrl = [NSURL URLWithString:@"http://h.hiphotos.baidu.com/zhidao/pic/item/38dbb6fd5266d0163e620ee5962bd40734fa3587.jpg"];
+        
+        // 下载图片
+        UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:imageUrl]];
+        
+        // 回到主线程中显示图片
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.imageView.image = image;
+        });
+        
+    });
 }
 
 @end
