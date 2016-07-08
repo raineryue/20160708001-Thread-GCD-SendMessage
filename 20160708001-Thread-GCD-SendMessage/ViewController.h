5c96158ad7872fdd94662cb9798f45a904365c79
Rainer <it_jinlei.yue@163.com>
2016-07-08 15:43:25 +0800

    Initial Commit

---
+++ 20160708001-Thread-GCD-SendMessage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20160708001-Thread-GCD-SendMessage
+//
+//  Created by Rainer on 16/7/8.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 20160708001-Thread-GCD-SendMessage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  20160708001-Thread-GCD-SendMessage
+//
+//  Created by Rainer on 16/7/8.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
